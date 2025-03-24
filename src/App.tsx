c8621eb1c835b107f67bf596faec5c5f837b3c2b
yocaji <yocaji@stone.sakura.ne.jp>
2025-03-24 20:11:37 +0900

    Enhance video section management: add clear video and refactor section handling

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,16 @@ function App() {
   const [videoHeight, setVideoHeight] = useState('auto');
   const [isRepeating, setIsRepeating] = useState(false);
   const [opts, setOpts] = useState({});
-  const [sections, setSections] = useState<{ key: number; startMinutes: number; startSeconds: number; endMinutes: number; endSeconds: number; videoId: string }[]>([]);
+  const [sections, setSections] = useState<
+    {
+      key: number;
+      startMinutes: number;
+      startSeconds: number;
+      endMinutes: number;
+      endSeconds: number;
+      videoId: string;
+    }[]
+  >([]);
   const [tempStartMinutes, setTempStartMinutes] = useState(0);
   const [tempStartSeconds, setTempStartSeconds] = useState(0);
   const [tempEndMinutes, setTempEndMinutes] = useState(0);
@@ -30,7 +39,14 @@ function App() {
       width: '100%',
       height: Number.parseInt(videoHeight) > 252 ? '252px' : videoHeight,
     });
-  }, [isRepeating, startMinutes, startSeconds, endMinutes, endSeconds, videoHeight]);
+  }, [
+    isRepeating,
+    startMinutes,
+    startSeconds,
+    endMinutes,
+    endSeconds,
+    videoHeight,
+  ]);
 
   useEffect(() => {
     const updateVideoHeight = () => {
@@ -55,7 +71,7 @@ function App() {
     const savedSections = [];
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
-      if (key && !isNaN(Number(key))) {
+      if (key && !Number.isNaN(Number(key))) {
         const section = JSON.parse(localStorage.getItem(key) || '{}');
         savedSections.push({ key: Number(key), ...section });
       }
@@ -88,6 +104,21 @@ function App() {
     setVideoId(tempVideoId);
   };
 
+  const handleClearVideo = () => {
+    setIsRepeating(false);
+    setActiveSectionKey(null);
+    setVideoId('');
+    setTempVideoId('');
+    setStartMinutes(0);
+    setStartSeconds(0);
+    setEndMinutes(0);
+    setEndSeconds(0);
+    setTempStartMinutes(0);
+    setTempStartSeconds(0);
+    setTempEndMinutes(0);
+    setTempEndSeconds(0);
+  };
+
   const setTimeToCurrent = async (
     setMinutes: React.Dispatch<React.SetStateAction<number>>,
     setSeconds: React.Dispatch<React.SetStateAction<number>>,
@@ -100,9 +131,6 @@ function App() {
   };
 
   const saveSection = () => {
-    const keys = Object.keys(localStorage).map(Number).filter((key) => !isNaN(key));
-    const maxKey = keys.length > 0 ? Math.max(...keys) : -1;
-    const newKey = maxKey + 1;
     const section = {
       startMinutes: tempStartMinutes,
       startSeconds: tempStartSeconds,
@@ -116,17 +144,36 @@ function App() {
       endMinutes: section.endMinutes,
       endSeconds: section.endSeconds,
     };
-    localStorage.setItem(`${newKey}`, JSON.stringify(sectionData));
 
-    setSections((prevSections) => [...prevSections, { key: newKey, ...section, videoId }]);
-
-    setTempStartMinutes(0);
-    setTempStartSeconds(0);
-    setTempEndMinutes(0);
-    setTempEndSeconds(0);
+    if (activeSectionKey === null) {
+      const keys = Object.keys(localStorage)
+        .map(Number)
+        .filter((key) => !Number.isNaN(key));
+      const maxKey = keys.length > 0 ? Math.max(...keys) : -1;
+      const newKey = maxKey + 1;
+      localStorage.setItem(`${newKey}`, JSON.stringify(sectionData));
+      const newSection = { key: newKey, ...section, videoId };
+      setSections((prevSections) => [...prevSections, newSection]);
+      loadSection(newSection);
+    } else {
+      localStorage.setItem(`${activeSectionKey}`, JSON.stringify(sectionData));
+      setSections((prevSections) =>
+        prevSections.map((sec) =>
+          sec.key === activeSectionKey
+            ? { key: activeSectionKey, ...section, videoId }
+            : sec,
+        ),
+      );
+    }
   };
 
-  const setSection = (section: { startMinutes: number; startSeconds: number; endMinutes: number; endSeconds: number, videoId: string }) => {
+  const setSection = (section: {
+    startMinutes: number;
+    startSeconds: number;
+    endMinutes: number;
+    endSeconds: number;
+    videoId: string;
+  }) => {
     setIsRepeating(true);
     setStartMinutes(section.startMinutes);
     setStartSeconds(section.startSeconds);
@@ -136,7 +183,14 @@ function App() {
     setTempVideoId(section.videoId);
   };
 
-  const loadSection = (section: { startMinutes: number; startSeconds: number; endMinutes: number; endSeconds: number, videoId: string, key: number }) => {
+  const loadSection = (section: {
+    startMinutes: number;
+    startSeconds: number;
+    endMinutes: number;
+    endSeconds: number;
+    videoId: string;
+    key: number;
+  }) => {
     setSection(section);
     setActiveSectionKey(section.key);
     setTempStartMinutes(section.startMinutes);
@@ -157,7 +211,9 @@ function App() {
 
   const deleteSection = (key: number) => {
     localStorage.removeItem(`${key}`);
-    setSections((prevSections) => prevSections.filter((section) => section.key !== key));
+    setSections((prevSections) =>
+      prevSections.filter((section) => section.key !== key),
+    );
   };
 
   return (
@@ -179,118 +235,146 @@ function App() {
         </div>
         <div className="m-4 px-3 w-full max-w-md">
           <div className="block">
-            <label className="text-sm font-bold">
+            <label htmlFor="videoId" className="text-sm font-bold">
               Video ID
             </label>
             <div className="flex items-center">
               <input
+                id="videoId"
                 type="text"
                 value={tempVideoId}
                 onChange={(e) => setTempVideoId(e.target.value)}
                 className={
                   'border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500'
                 }
-                disabled={isRepeating}
+                disabled={videoId !== ''}
               />
-              {!isRepeating && (
-                <button
-                  type={'button'}
-                  onClick={handleLoadVideo}
-                  className="ml-2 p-2 rounded bg-black text-white"
-                >
-                  Load video
-                </button>
-              )}
-            </div>
-          </div>
-          <div className="mt-2">
-            <div className="block">
-              <label className="text-sm font-bold">
-                Start from
-              </label>
-              <div className="flex items-center">
-                <input
-                  type="tel"
-                  value={tempStartMinutes}
-                  onChange={(e) => setTempStartMinutes(Number(e.target.value))}
-                  className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
-                />
-                <span className="mx-1">:</span>
-                <input
-                  type="tel"
-                  value={tempStartSeconds}
-                  onChange={(e) => setTempStartSeconds(Number(e.target.value))}
-                  className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
-                />
-                <button
-                  type={'button'}
-                  onClick={() => setTimeToCurrent(setTempStartMinutes, setTempStartSeconds)}
-                  className="ml-2 p-2 rounded bg-black text-white"
-                >
-                  Now
-                </button>
-              </div>
-            </div>
-            <div className="block mt-2">
-              <label className="text-sm font-bold">
-                End at
-              </label>
-              <div className="flex items-center">
-                <input
-                  type="tel"
-                  value={tempEndMinutes}
-                  onChange={(e) => setTempEndMinutes(Number(e.target.value))}
-                  className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
-                />
-                <span className="mx-1">:</span>
-                <input
-                  type="tel"
-                  value={tempEndSeconds}
-                  onChange={(e) => setTempEndSeconds(Number(e.target.value))}
-                  className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
-                />
-                <button
-                  type={'button'}
-                  onClick={() => setTimeToCurrent(setTempEndMinutes, setTempEndSeconds)}
-                  className="ml-2 p-2 rounded bg-black text-white"
-                >
-                  Now
-                </button>
-              </div>
-            </div>
-            <div className="flex space-x-2 mt-4">
               <button
                 type={'button'}
-                onClick={() => saveSection()}
-                className="p-2 w-full rounded bg-black text-white disabled:bg-gray-600 disabled:text-gray-400"
+                onClick={videoId ? handleClearVideo : handleLoadVideo}
+                className="ml-2 p-2 rounded bg-black text-white"
               >
-                Save Section
-              </button>
-              <button
-                type={'button'}
-                onClick={() => clearSection()}
-                className="p-2 w-full rounded bg-black text-white disabled:bg-gray-600 disabled:text-gray-400"
-              >
-                Clear
+                {videoId ? 'Clear' : 'Load'}
               </button>
             </div>
           </div>
+          {videoId && (
+            <div className="mt-2">
+              <div className="block">
+                <span className="text-sm font-bold">Start from</span>
+                <div className="flex items-center">
+                  <label htmlFor="startMinutes" className="sr-only">
+                    Start Minutes
+                  </label>
+                  <input
+                    id="startMinutes"
+                    type="tel"
+                    value={tempStartMinutes}
+                    onChange={(e) =>
+                      setTempStartMinutes(Number(e.target.value))
+                    }
+                    className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
+                  />
+                  <span className="mx-1">:</span>
+                  <label htmlFor="startSeconds" className="sr-only">
+                    Start Seconds
+                  </label>
+                  <input
+                    id="startSeconds"
+                    type="tel"
+                    value={tempStartSeconds}
+                    onChange={(e) =>
+                      setTempStartSeconds(Number(e.target.value))
+                    }
+                    className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
+                  />
+                  <button
+                    type={'button'}
+                    onClick={() =>
+                      setTimeToCurrent(setTempStartMinutes, setTempStartSeconds)
+                    }
+                    className="ml-2 p-2 rounded bg-black text-white"
+                  >
+                    Now
+                  </button>
+                </div>
+              </div>
+              <div className="block mt-2">
+                <span className="text-sm font-bold">End at</span>
+                <div className="flex items-center">
+                  <label htmlFor="endMinutes" className="sr-only">
+                    End Minutes
+                  </label>
+                  <input
+                    id="endMinutes"
+                    type="tel"
+                    value={tempEndMinutes}
+                    onChange={(e) => setTempEndMinutes(Number(e.target.value))}
+                    className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
+                  />
+                  <span className="mx-1">:</span>
+                  <label htmlFor="endSeconds" className="sr-only">
+                    End Seconds
+                  </label>
+                  <input
+                    id="endSeconds"
+                    type="tel"
+                    value={tempEndSeconds}
+                    onChange={(e) => setTempEndSeconds(Number(e.target.value))}
+                    className="border rounded p-2 w-full border-gray-300 disabled:bg-gray-200 disabled:text-gray-500"
+                  />
+                  <button
+                    type={'button'}
+                    onClick={() =>
+                      setTimeToCurrent(setTempEndMinutes, setTempEndSeconds)
+                    }
+                    className="ml-2 p-2 rounded bg-black text-white"
+                  >
+                    Now
+                  </button>
+                </div>
+              </div>
+              <div className="flex space-x-2 mt-4">
+                <button
+                  type={'button'}
+                  onClick={() => saveSection()}
+                  className="p-2 w-full rounded bg-black text-white disabled:bg-gray-600 disabled:text-gray-400"
+                >
+                  Save Section
+                </button>
+                <button
+                  type={'button'}
+                  onClick={() => clearSection()}
+                  className="p-2 w-full rounded bg-black text-white disabled:bg-gray-600 disabled:text-gray-400"
+                >
+                  Reset Section
+                </button>
+              </div>
+            </div>
+          )}
           {sections.length > 0 && (
             <div className="mt-5">
               <hr className="my-2 border-gray-300" />
               <h2 className="mt-5 text-sm font-bold">Sections</h2>
               {sections.map((section) => (
-                <div key={section.key}
-                     className={`py-2 flex items-center space-x-2 ${activeSectionKey === section.key ? 'bg-gray-200' : ''}`}>
-                  <div>{section.key}: {section.videoId}</div>
-                  <div>{section.startMinutes}:{section.startSeconds} - {section.endMinutes}:{section.endSeconds}</div>
+                <div
+                  key={section.key}
+                  className={`py-2 flex items-center space-x-2 ${activeSectionKey === section.key ? 'bg-gray-200' : ''}`}
+                >
+                  <div>
+                    {section.key}: {section.videoId}
+                  </div>
+                  <div>
+                    {section.startMinutes}:{section.startSeconds} -{' '}
+                    {section.endMinutes}:{section.endSeconds}
+                  </div>
                   <div className="ml-auto flex space-x-2">
                     <button
                       type={'button'}
                       onClick={() => loadSection(section)}
                       className="p-2 rounded bg-black text-white"
                     >
-                      Load
+                      Set
                     </button>
                     <button
                       type={'button'}
